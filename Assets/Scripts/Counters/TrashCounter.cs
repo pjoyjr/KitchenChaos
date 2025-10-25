5da9007de68485a944e09768de35231988ddd531
pjoyjr <37516097+pjoyjr@users.noreply.github.com>
2025-10-25 01:02:50 -0400

    Pause Screen and clearing statics

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -7,13 +7,18 @@ public class TrashCounter : BaseCounter {
     
     public static event EventHandler OnAnyObjectTrashed;
 
+    new public static void ResetStaticData() {
+        OnAnyObjectTrashed = null;
+    }
+
     public override void Interact(Player player) {
         if (player.HasKitchenObject()) {
             // Player is carrying something
             player.GetKitchenObject().DestroySelf();
 
             OnAnyObjectTrashed?.Invoke(this, EventArgs.Empty);
-        } else {
+        }
+        else {
             // Player is not carrying anything
         }
     }
--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -8,6 +8,8 @@ public class KitchenGameManager : MonoBehaviour {
     public static KitchenGameManager Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
 
     private enum State {
         WaitingToStart,
@@ -21,13 +23,20 @@ public class KitchenGameManager : MonoBehaviour {
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
     private float gamePlayingTimerMax = 10f;
+    private bool isGamePaused = false;
 
     private void Awake() {
         Instance = this;
+
+        state = State.WaitingToStart;
     }
 
     private void Start() {
-        state = State.WaitingToStart;
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
+    }
+
+    private void GameInput_OnPauseAction(object sender, EventArgs e) {
+        TogglePauseGame();
     }
 
     private void Update() {
@@ -81,4 +90,14 @@ public class KitchenGameManager : MonoBehaviour {
         return gamePlayingTimer / gamePlayingTimerMax;
     }
 
+    public void TogglePauseGame() {
+        isGamePaused = !isGamePaused;
+        if(isGamePaused) {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        } else {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
